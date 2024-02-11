775e91b23591f89efb76198cff63350d2cc08709
kouroshalastibusiness <kouroshalastibusiness@gmail.com>
2024-02-11 10:01:02 +0000

    Created Thread Controller Schema

---
+++ backend/src/controller/thread.controller.js
@@ -0,0 +1,8 @@
+import Thread from "../models/thread.model";
+
+// TODO
+
+export const createThread = async (req, res, next) => {}
+export const getThreads = async (req, res, next) => {}
+export const deleteThread = async (req, res, next) => {}
+export const updateThread = async (req, res, next) => {}
