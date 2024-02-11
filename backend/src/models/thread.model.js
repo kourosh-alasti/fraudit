933b350f96ac09c80a4085a6f7bdb847269441ac
kouroshalastibusiness <kouroshalastibusiness@gmail.com>
2024-02-11 10:00:47 +0000

    Created Thread Model for subfraudits

---
+++ backend/src/models/thread.model.js
@@ -0,0 +1,29 @@
+const { default: mongoose } = 'mongoose';
+
+const threadSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true, 
+        unique: true,
+    }, 
+    user_id: {
+        type: String,
+        required: true,
+    },
+    fraudit_id: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    }, 
+    
+}, {timestamps: true})
+
+const Thread = mongoose.model('Thread', threadSchema);
+export default Thread;
