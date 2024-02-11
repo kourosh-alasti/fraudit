e4a9be7c173dae6ebf2a3817147360e100892a4e
Kourosh Alasti <coding@kouroshalasti.com>
2024-02-10 20:06:09 -0800

    comment model
    updating comment model

--- backend/src/models/comment.model.js
+++ backend/src/models/comment.model.js
@@ -8,4 +8,5 @@ const commentSchema = mongoose.Schema({
   posted_at: Date,
 });
 
-export const Comment = mongoose.model("Comment", commentSchema);
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
