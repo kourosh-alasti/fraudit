4a5b7d0d32ad16556724c18bfcd253af80f15bfc
kouroshalastibusiness <kouroshalastibusiness@gmail.com>
2024-02-12 10:41:33 +0000

    fix test routes

--- backend/src/routes/test.route.js
+++ backend/src/routes/test.route.js
@@ -1,48 +1,11 @@
 const express = require("express");
 const Logger = require("../utils/logger");
+const { testLogger, test } = require("../controllers/test.controller");
+
 const router = express.Router();
 const logger = new Logger("app");
 
-router.get('/test', (req, res) => {
-  return res.send(
-    {
-      success: true,
-      message: "Hello World"
-    }
-  )
-})
-
-router.post("/test", (req, res) => {
-  let error = {};
-
-  const body = req.body;
-
-  logger.setLoggerData(body);
-  logger.info("Request received at /test", req.body);
-
-  if (body.name === null || body.name === "") {
-    logger.error("Name Field is Empty");
-    error["name"] = "Name Field is Empty";
-  }
-
-  if (body.age === null || body.age === "") {
-    logger.error("Age Field is Empty");
-    error["age"] = "Age Field is Empty";
-  }
-
-  if (Object.keys(error).length !== 0) {
-    logger.error("Returning Error Response", {
-      success: false,
-    });
-
-    res.send("ERROR");
-  } else {
-    logger.info("Returning Success Response", {
-      success: true,
-    });
-
-    res.send("No Error");
-  }
-});
+router.get('/test', test)
+router.post("/test", testLogger);
 
 module.exports = router;
