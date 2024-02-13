d27069346bd4183c5c73efbda5f7a9a5f79d8a17
Kourosh Alasti <coding@kouroshalasti.com>
2024-02-12 20:10:45 -0800

    migrate membership model

---
+++ backend/src/models/member.model.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require("mongoose");
+
+const memberSchema = mongoose.Schema(
+  {
+    frauditId: { type: String, required: true },
+    userId: { type: String, required: true },
+    // "member", "admin", "owner"
+    permissionLevel: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Member = mongoose.model("Member", memberSchema);
+module.exports = Member;
