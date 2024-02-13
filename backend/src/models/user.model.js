7e3ff1de101fc2d7c03d34062385cc1285af6c3c
Kourosh Alasti <coding@kouroshalasti.com>
2024-02-12 18:55:47 -0800

    Update user.model.js
    updating user model to add admin permissions globally

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -26,10 +26,15 @@ var userSchema = mongoose.Schema(
       default:
         "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
   },
   { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
