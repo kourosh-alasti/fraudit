9a4301dc32e098e8e34b249cdaa9861cf6ef7879
Kourosh Alasti <coding@kouroshalasti.com>
2024-02-12 19:02:26 -0800

    Update university.model.js
    added address and rating to university model

--- backend/src/models/university.model.js
+++ backend/src/models/university.model.js
@@ -1,15 +1,33 @@
-const { default: mongoose} = require('mongoose');
+const { default: mongoose } = require("mongoose");
 
-var universitySchema = mongoose.Schema({
+var universitySchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     abbreviation: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-}, {timestamps: true})
+    overallRating: {
+      type: Number,
+      required: true,
+      default: 5,
+    },
+    address: {
+      type: Object,
+      required: true,
+      default: {
+        street: null,
+        city: null,
+        state: null,
+        zup: null,
+      },
+    },
+  },
+  { timestamps: true }
+);
 
 const University = mongoose.model("University", universitySchema);
-module.exports = University
+module.exports = University;
