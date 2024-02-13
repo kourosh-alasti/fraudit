e588535bb5cf801794687b425c2781f7f53345e7
Kourosh Alasti <coding@kouroshalasti.com>
2024-02-12 18:57:18 -0800

    Update auth.js
    created verifyIsAdmin middleware to prevent request if user is not admin

--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -1,31 +1,39 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+const { getUserInformation } = require("../database/queries/user.query/");
 
 const verifyToken = (req, res, next) => {
-    /*
-     *  PULLS JWT TOKEN FROM COOKIES
-     */
-    const token = req.cookies.access_token;
+  /*
+   *  PULLS JWT TOKEN FROM COOKIES
+   */
+  const token = req.cookies.access_token;
 
-    /*
-     * CHECKS IF TOKEN EXISTS
-     */    
-    if(!token) {
-        // TODO: ERROR HANDLER
-        return next();
+  /*
+   * CHECKS IF TOKEN EXISTS
+   */
+  if (!token) {
+    // TODO: ERROR HANDLER
+    return next();
+  }
+
+  /*
+   * VERIFIES TOKEN AND SETS REQ.USER
+   */
+  jwt.verify(token, process.env.DEV_JWT_SECRET, (err, user) => {
+    if (err) {
+      // TODO: ERROR HANDLER
+      return next();
     }
 
-    /*
-     * VERIFIES TOKEN AND SETS REQ.USER
-     */
-    jwt.verify(token, process.env.DEV_JWT_SECRET, (err, user) => {
-        if(err) {
-            // TODO: ERROR HANDLER
-            return next();
-        }
+    req.user = user;
+    next();
+  });
+};
 
-        req.user = user;
-        next();
-    })
-}
+const verifyIsAdmin = (req, res, next) => {
+  const userId = req.user.id;
+
+  const userInfo = getUserInformation(userId);
+};
 
 module.exports.verifyToken = verifyToken;
+module.exports.verifyIsAdmin = verifyIsAdmin;
