a65f925280d9909845299f97b9ecf90ea7cadd65
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-02-14 03:14:41 -0800

    Update user.route.js
    linter fixes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const { deleteUser, getUser, getUsers, logout, updateUser} = require('../controllers/user.controller')
-const {verifyToken } = require('../utils/auth')
+const express = require("express");
+const {
+  deleteUser,
+  getUser,
+  getUsers,
+  logout,
+  updateUser,
+} = require("../controllers/user.controller");
+const { verifyToken } = require("../utils/auth");
 
-const router = express.Router()
+const router = express.Router();
 
-router.put('/update/:userId', verifyToken, updateUser);
-router.delete('/delete/:userId', verifyToken, deleteUser);
-router.post('/logout', logout);
-router.get('/getusers', verifyToken, getUsers);
-router.get('/:userId', getUser);
+router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
+router.post("/logout", logout);
+router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
 
 module.exports = router;
-
