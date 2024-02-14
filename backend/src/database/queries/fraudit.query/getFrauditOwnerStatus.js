3bc5955421ea6854b999a1f6c9793c8375e9eed8
Kourosh Alasti <coding@kouroshalasti.com>
2024-02-13 21:18:45 -0800

    Create getFrauditOwnerStatus.js
    added getFrauditOwnerStatus query

---
+++ backend/src/database/queries/fraudit.query/getFrauditOwnerStatus.js
@@ -0,0 +1,17 @@
+const { getFrauditInformation } = require(".");
+const { getAdminStatus } = require("../user.query");
+
+const getFrauditOwnerStatus = async (frauditId, userId) => {
+  const fraudit = getFrauditInformation(frauditId);
+
+  if (fraudit.ownerId !== userId) {
+    //TODO: ERROR HANDLER
+    throw new Error("You are not the owner of this fraudit");
+  }
+
+  return {
+    isOwner: fraudit.ownerId === userId || getAdminStatus(userId),
+  };
+};
+
+module.exports = getFrauditOwnerStatus;
