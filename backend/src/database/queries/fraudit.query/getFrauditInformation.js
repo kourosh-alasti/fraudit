aedebf487831d78e96737c8bbe531045d0b21bd8
Kourosh Alasti <coding@kouroshalasti.com>
2024-02-13 20:09:37 -0800

    Create getFrauditInformation.js
    fraudit info query

---
+++ backend/src/database/queries/fraudit.query/getFrauditInformation.js
@@ -0,0 +1,18 @@
+const Fraudit = require("../../../models/fraudit.model");
+
+const getFrauditInformation = async (id) => {
+  const fraudit = await Fraudit.findById(id);
+
+  if (!fraudit) {
+    // TODO: ERRO HANDLER
+    throw new Error("Fraudit Does not exist");
+  }
+
+  const { ...info } = fraudit._doc;
+
+  return {
+    ...info,
+  };
+};
+
+module.exports = getFrauditInformation;
