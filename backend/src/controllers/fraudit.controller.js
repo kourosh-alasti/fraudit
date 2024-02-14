42c16301b7c1c66669de3225125814052863164f
Kourosh Alasti <coding@kouroshalasti.com>
2024-02-13 20:09:28 -0800

    Create fraudit.controller.js
    started fraudit routes controller

---
+++ backend/src/controllers/fraudit.controller.js
@@ -0,0 +1,54 @@
+const Fraudit = require("../models/fraudit.model");
+
+const createFraudit = async (req, res, next) => {
+  const { title, description, slug, userId } = req.body;
+
+  if (
+    !title ||
+    !description ||
+    !slug ||
+    !userId ||
+    title === "" ||
+    description === "" ||
+    slug === "" ||
+    userId === ""
+  ) {
+    // TODO: ERROR HANDLER
+    next();
+  }
+
+  const newFraudit = new Fraudit({
+    title,
+    description,
+    slug,
+    ownerId: userId,
+    memberCount: 1,
+  });
+
+  try {
+    await newFraudit.save();
+    res.status(201).json("Created SubFraudit Successfully");
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteFraudit = async (req, res, next) => {
+  const { frauditId, userId } = req.body;
+
+  const { isOwner } = getFrauditOwnerStatus(frauditId, userId);
+
+  if (!isOwner) {
+    // TODO: ERROR HANDLER
+    next();
+  }
+
+  try {
+    await Fraudit.findByIdAndDelete(frauditId);
+    res.status(200).json("SubFraudit has been successfully deleted");
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.createFraudit === createFraudit;
