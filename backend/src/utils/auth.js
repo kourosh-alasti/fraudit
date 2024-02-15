180f672bd02cf8a31fa9a1744519ae4cd98100c2
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-02-15 03:46:11 -0800

    Update auth.js
    added verifyIsAdmin middleware

--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -29,10 +29,18 @@ const verifyToken = (req, res, next) => {
   });
 };
 
-const verifyIsAdmin = (req, res, next) => {
+const verifyIsAdmin = async (req, res, next) => {
   const userId = req.user.id;
 
-  const userInfo = getUserInformation(userId);
+  const userInfo = await getUserInformation(userId);
+
+  if (!userInfo.isAdmin) {
+    req.isAdmin = false;
+    next();
+  }
+
+  req.isAdmin = true;
+  next();
 };
 
 module.exports.verifyToken = verifyToken;
