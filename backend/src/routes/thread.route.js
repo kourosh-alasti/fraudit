68a983aa356e6161e09e2f240ae9b53dba6d4ebd
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-02-15 03:47:34 -0800

    Update thread.route.js
    eslint fixes

--- backend/src/routes/thread.route.js
+++ backend/src/routes/thread.route.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const { verifyToken } = require('../utils/auth');
-const { createThread, getThreads, deleteThread, updateThread } = require('../controllers/thread.controller');
+const express = require("express");
+const { verifyToken } = require("../utils/auth");
+const {
+  createThread,
+  getThreads,
+  deleteThread,
+  updateThread,
+} = require("../controllers/thread.controller");
 
 const router = express.Router();
 
-router.post('/create', verifyToken, createThread);
-router.get('/get', getThreads );
-router.delete('/delete/:threadId/:userId', verifyToken, deleteThread)
-router.put('/update/:threadId/:userId', verifyToken, updateThread)
+router.post("/create", verifyToken, createThread);
+router.get("/get", getThreads);
+router.delete("/delete/:threadId", verifyToken, deleteThread);
+router.put("/update/:threadId", verifyToken, updateThread);
 
 module.exports = router;
