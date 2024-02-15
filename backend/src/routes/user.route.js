97f35a6adccb897f39ded506ccaa1558c6ba7f1a
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-02-15 03:47:58 -0800

    Update user.route.js
    added new route /admin/:userId and implemented new middleware

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -5,15 +5,17 @@ const {
   getUsers,
   logout,
   updateUser,
+  updateUserAdmin,
 } = require("../controllers/user.controller");
-const { verifyToken } = require("../utils/auth");
+const { verifyToken, verifyIsAdmin } = require("../utils/auth");
 
 const router = express.Router();
 
 router.put("/update/:userId", verifyToken, updateUser);
+router.put("/admin/:userId", verifyToken, verifyIsAdmin, updateUserAdmin);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/logout", logout);
-router.get("/getusers", verifyToken, getUsers);
-router.get("/:userId", getUser);
+router.get("/getusers", verifyToken, verifyIsAdmin, getUsers);
+router.get("/:userId", verifyToken, getUser);
 
 module.exports = router;
