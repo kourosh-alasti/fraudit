6244f5e143e1e4466d6696c5643eea710d34da0d
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-02-17 22:38:27 -0800

    Update auth.js
    debug logs

--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -1,17 +1,20 @@
 const jwt = require('jsonwebtoken')
 const { getUserInformation } = require('../database/queries/user.query/')
+const { debug, warn, error } = require('./consoler')
 
 const verifyToken = (req, res, next) => {
   /*
    *  PULLS JWT TOKEN FROM COOKIES
    */
   const token = req.cookies.access_token
+  debug(`ACCESS TOKEN: ${token}`)
 
   /*
    * CHECKS IF TOKEN EXISTS
    */
   if (!token) {
     // TODO: ERROR HANDLER
+    warn('Token does not exist')
     return next()
   }
 
@@ -19,11 +22,14 @@ const verifyToken = (req, res, next) => {
    * VERIFIES TOKEN AND SETS REQ.USER
    */
   jwt.verify(token, process.env.DEV_JWT_SECRET, (err, user) => {
+    debug('Verifying token')
     if (err) {
       // TODO: ERROR HANDLER
+      error('Error while trying to verify Token')
       return next()
     }
 
+    debug('Setting User')
     req.user = user
     next()
   })
@@ -31,14 +37,17 @@ const verifyToken = (req, res, next) => {
 
 const verifyIsAdmin = async (req, res, next) => {
   const userId = req.user.id
+  debug(`USER ID: ${userId} `)
 
   const userInfo = await getUserInformation(userId)
 
   if (!userInfo.isAdmin) {
+    warn('Not an admin')
     req.isAdmin = false
     next()
   }
 
+  debug('Is Admin')
   req.isAdmin = true
   next()
 }
