09bd050349a478a4a873361cc1033259b02cb0b2
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-02-20 17:59:17 -0800

    Update thread.controller.js
    started dev for thread controller

--- backend/src/controllers/thread.controller.js
+++ backend/src/controllers/thread.controller.js
@@ -1,11 +1,44 @@
-// const Thread = require('../models/thread.model')
+/* eslint-disable camelcase */
+const Thread = require('../models/thread.model')
 
 // TODO
 
 const createThread = async (req, res, next) => {}
 const getThreads = async (req, res, next) => {}
-const getThread = async (req, res, next) => {}
-const deleteThread = async (req, res, next) => {}
+
+const getThread = async (req, res, next) => {
+  const { fraudit_id } = req.body
+  const user_id = req.user.id
+
+  try {
+    const thread = await Thread.findOne({ fraudit_id, user_id }).exec()
+
+    if (!thread) {
+      // ERROR HANDLER
+      return next()
+    }
+
+    const { ...rest } = thread._doc
+
+    res.status(200).json(rest)
+  } catch (err) {
+    next(err)
+  }
+}
+
+const deleteThread = async (req, res, next) => {
+  if (!req.isAdmin) {
+    // ERROR HANDLER
+    return next()
+  }
+
+  try {
+    await Thread.findOne().exec()
+    res.status(200).json('Thread has been deleted')
+  } catch (err) {
+    next(err)
+  }
+}
 const updateThread = async (req, res, next) => {}
 
 module.exports.createThread = createThread
