42f44e33e865f3568d3b5b80d21ea15f315ffa83
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-02-22 04:45:32 -0800

    update root layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex flex-col min-h-screen min-w-screen h-screen w-screen">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
