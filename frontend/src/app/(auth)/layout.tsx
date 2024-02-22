bb939bb800263449562429db9dd71f6da2f85f8d
Kourosh Alasti <coding@kouroshalasti.com>
2024-02-22 15:14:45 -0800

    Update layout.tsx
    comment imports

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,11 +1,11 @@
-import UIFooter from "@/components/global/Footer";
+// import UIFooter from "@/components/global/Footer";
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col justify-center items-center h-full w-full">
       {children}
-      <UIFooter />
+      {/* <UIFooter /> */}
     </div>
   );
 };
