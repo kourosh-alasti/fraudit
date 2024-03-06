93a17ee305dd71799dbac459a0d722f8d43970e9
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-03-06 04:15:26 -0800

    Update user.controller.js
    update user controller for testing

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -15,6 +15,8 @@ const getUser = async (req, res, next) => {
      */
     if (!user) {
       // TODO: ERROR HANDLING
+      res.status(404)
+      res.json({ message: 'User Not Found' })
       return next()
     }
 
@@ -26,7 +28,8 @@ const getUser = async (req, res, next) => {
     /*
      * RETURN STATUS CODE 200 FOR OK AND USER DATA AS JSON
      */
-    res.status(200).json(rest)
+    res.status(200)
+    res.json(rest)
   } catch (err) {
     // ERROR HANDLING
     next(err)
@@ -40,7 +43,8 @@ const getUsers = async (req, res, next) => {
    */
   if (!req.isAdmin) {
     // TODO: ERROR HANDLER
-    res.status(403).json({
+    res.status(403)
+    res.json({
       message: 'Access Denied, only Site Admins can access this information'
     })
 
@@ -97,10 +101,15 @@ const getUsers = async (req, res, next) => {
      * Returns status 200 for success
      * Returns users as JSON object
      */
-    res.status(200).json({
+    // res.status(200).json({
+    //   users: usersWithoutPassword,
+    //   totalUsers
+    //   // lastMonthUsers,
+    // })
+    res.status(200)
+    res.json({
       users: usersWithoutPassword,
       totalUsers
-      // lastMonthUsers,
     })
   } catch (err) {
     next(err)
@@ -111,16 +120,16 @@ const getUsers = async (req, res, next) => {
 const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId && !req.isAdmin) {
     // TODO: ERROR HANDLER
-    res
-      .status(403)
-      .json({ message: 'Access Denied. Only an Admin can delete accounts' })
+    res.status(403)
+    res.json({ message: 'Access Denied. Only an Admin can delete accounts' })
 
     return next()
   }
 
   try {
     await User.findByIdAndDelete(req.params.userId)
-    res.status(200).json({ message: 'Your account has been deleted' })
+    res.status(200)
+    res.json({ message: 'Your account has been deleted' })
   } catch (err) {
     next(err)
   }
@@ -130,9 +139,8 @@ const deleteUser = async (req, res, next) => {
 const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId && !req.isAdmin) {
     // TODO: ERROR HANDLER
-    res
-      .status(403)
-      .json({ message: "Access Denied. Can't update other accounts" })
+    res.status(403)
+    res.json({ message: "Access Denied. Can't update other accounts" })
     return next()
   }
 
@@ -182,7 +190,8 @@ const updateUser = async (req, res, next) => {
     )
 
     const { password, ...rest } = updatedUser._doc
-    res.status(200).json(rest)
+    res.status(200)
+    res.json(rest)
   } catch (err) {
     next(err)
   }
@@ -192,11 +201,10 @@ const updateUser = async (req, res, next) => {
 const updateUserAdmin = async (req, res, next) => {
   if (!req.isAdmin) {
     // TODO: ERROR HANDLER
-    res
-      .status(403)
-      .json({
-        message: 'Access Denied. Do Not have permission to update admin status'
-      })
+    res.status(403)
+    res.json({
+      message: 'Access Denied. Do Not have permission to update admin status'
+    })
 
     return next()
   }
@@ -220,7 +228,8 @@ const updateUserAdmin = async (req, res, next) => {
     )
 
     const { password, ...rest } = updatedUser._doc
-    res.status(200).json(rest)
+    res.status(200)
+    res.json(rest)
   } catch (err) {
     next(err)
   }
