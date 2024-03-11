310d5eee4c238b3244175e275c4c89fa1185c76a
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-03-11 02:26:22 -0700

    Update Navbar.tsx
    update navbar for new layout

--- frontend/src/components/global/Navbar.tsx
+++ frontend/src/components/global/Navbar.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Menu } from "lucide-react";
+import { ChevronLeft, Menu } from "lucide-react";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 import Image from "next/image";
@@ -10,6 +10,9 @@ import { Button } from "../ui/button";
 import { useUserStore } from "@/store/use-user-store";
 import { useToast } from "../ui/use-toast";
 import { useRouter } from "next/navigation";
+import { SidebarContext } from "@/providers/sidebar-provider";
+import NewSidebar from "./NewSidebar";
+import { Input } from "../ui/input";
 
 const UINavbar = () => {
   const [isMobileMenu, setIsMobileMenu] = useState(false);
@@ -17,12 +20,10 @@ const UINavbar = () => {
   const router = useRouter();
   const user = useUserStore((state) => state.user);
   const { toast } = useToast();
+  const sidebarContext = useContext(SidebarContext);
+  const { toggled, toggleSidebar } = sidebarContext;
 
-  const MENU_ITEMS = [
-    { title: "Home", path: "/" },
-    { title: "Fraudits", path: "/fraudits" },
-    { title: "Profile", path: "/profile" },
-  ];
+  const MENU_ITEMS = [{ title: "Profile", path: "/profile" }];
 
   const onLogout = async () => {
     try {
@@ -55,9 +56,14 @@ const UINavbar = () => {
   };
 
   return (
-    <nav className="bg-white w-full border-b md:border-0 mb-4">
-      <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:justify-between md:px-8">
-        <div className="flex items-center justify-between py-3 md:py-5 md:block">
+    <nav className=" w-full border-b mb-2">
+      <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:justify-between  md:px-8">
+        <div className="flex items-center justify-between py-3 md:py-5 ">
+          <ChevronLeft
+            onClick={toggleSidebar}
+            className="hover:cursor-pointer"
+          />
+          <NewSidebar />
           <Link href="/">
             {/* <h1 className="text-3xl font-bold text-purple-600">Fraudit</h1> */}
             <Image
@@ -67,6 +73,11 @@ const UINavbar = () => {
               alt="Fraudit Logo"
             />
           </Link>
+          <Input
+            type="text"
+            placeholder="Search"
+            className="w-96 px-2 py-2 text-gray-500 bg-transparent hidden md:block rounded-md border-slate-500"
+          />
           <div className="md:hidden">
             <button
               className="text-gray-700 outline-none p-2 rounded-md focuse:border-gray-400 focus:border"
@@ -77,37 +88,26 @@ const UINavbar = () => {
           </div>
         </div>
         <div
-          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+          className={`flex-1 justify-self-center w-full pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
             isMobileMenu ? "block" : "hidden"
           }`}
         >
-          <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-            {MENU_ITEMS.map((item, index) => (
-              <li key={index} className="text-gray-600 hover:text-indigo-600">
-                {item.title === "Profile" ? (
-                  <Link href={item.path}>
-                    <p className="md:hidden block">{item.title}</p>
-                    <Avatar className="h-14 w-14 hidden md:block">
-                      <AvatarImage
-                        src={
-                          user
-                            ? (user.profile_picture as string)
-                            : "https://github.com/shadcn.png"
-                        }
-                      />
-                      <AvatarFallback>
-                        {user
-                          ? user.first_name[0].toUpperCase() +
-                            user.last_name[0].toUpperCase()
-                          : "TU"}
-                      </AvatarFallback>
-                    </Avatar>
-                  </Link>
-                ) : (
-                  <Link href={item.path}>{item.title}</Link>
-                )}
-              </li>
-            ))}
+          <ul className="justify-center w-full items-center space-y-8 flex flex-col md:space-x-6 md:space-y-0">
+            <Input
+              type="text"
+              placeholder="Search"
+              className="w-96 px-2 py-2 text-gray-500 bg-transparent md:hidden rounded-md border-slate-500"
+            />
+            <li key="user-avatar" className="text-gray-600 self-end ">
+              <Link href="/profile">
+                <Avatar className="h-14 w-14 hidden md:block">
+                  <AvatarImage src={user?.profile_picture as string} />
+                  <AvatarFallback>
+                    {`${user?.first_name[0].toUpperCase()}${user?.last_name[0].toUpperCase()}`}
+                  </AvatarFallback>
+                </Avatar>
+              </Link>
+            </li>
             <li key="logout-user" className="md:hidden block">
               <Button onClick={onLogout}>Logout</Button>
             </li>
