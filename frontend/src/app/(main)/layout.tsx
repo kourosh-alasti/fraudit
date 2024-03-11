70b67efc7337f15b2889bcbe907564c991a70855
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-03-11 02:23:51 -0700

    Update layout.tsx
    update main layout

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,18 +1,15 @@
 import UIFooter from "@/components/global/Footer";
-import FrauditsSidebar from "@/components/global/FrauditsSidebar";
 import UINavbar from "@/components/global/Navbar";
 import { Toaster } from "@/components/ui/toaster";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <Toaster />
-      <UINavbar />
-      <div className="flex">
-        <FrauditsSidebar />
-        {children}
+    <div className="flex justify-center w-full h-full">
+      <div className="flex flex-1 flex-col w-full">
+        <Toaster />
+        <UINavbar />
+        <div className="px-8 w-full">{children}</div>
       </div>
-      <UIFooter />
     </div>
   );
 };
