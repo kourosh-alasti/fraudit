4f173dc667948cb94a539cad1216be56545f6b21
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-11 05:02:58 -0700

    Update layout.tsx
    create root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <html lang="en">
+        <body className={inter.className}>
+          <Toaster />
+          <div className="flex h-full w-full flex-col">{children}</div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
