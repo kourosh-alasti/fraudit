6f309c1ea76a750de7a314dff4d23bb1d2e64cc8
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-16 01:59:55 -0700

    Update feed-wrapper.tsx
    prettier

--- src/components/feed-wrapper.tsx
+++ src/components/feed-wrapper.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export const FeedWrapper = ({ children }: Props) => {
   return (
-    <div className="flex-1 relative top-0 pb-10 flex flex-col gap-6">
+    <div className="relative top-0 flex flex-1 flex-col gap-6 pb-10">
       {children}
     </div>
   );
