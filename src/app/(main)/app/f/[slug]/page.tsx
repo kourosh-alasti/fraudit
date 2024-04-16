8d269e287e17138e85adb49d7e723963e8720a74
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-16 02:01:00 -0700

    Update page.tsx
    simplify page component

--- src/app/(main)/app/f/[slug]/page.tsx
+++ src/app/(main)/app/f/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
+import { CreateThreadCard } from "@/components/fraudit/create-thread-card";
 import { FrauditInfo } from "@/components/fraudit/fraudit-info";
-import { ThreadItem } from "@/components/fraudit/thread-item";
+import { PostInfo } from "@/components/fraudit/post-info";
+import { StickyWrapper } from "@/components/sticky-wrapper";
 
 import React from "react";
 
@@ -9,17 +11,13 @@ const FrauditPage = ({ params: { slug } }: { params: { slug: string } }) => {
 
   return (
     <div className="flex flex-col gap-[48px] px-6 md:flex-row-reverse">
-      <FrauditInfo slug={slug} />
+      <StickyWrapper>
+        <FrauditInfo slug={slug} />
+        <CreateThreadCard />
+      </StickyWrapper>
       <FeedWrapper>
-        <ThreadItem />
-        <ThreadItem />
-        <ThreadItem />
-        <ThreadItem />
-        <ThreadItem />
-        <ThreadItem />
-        <ThreadItem />
-        <ThreadItem />
-        <ThreadItem />
+        <FrauditInfo slug={slug} className="w-full" />
+        <PostInfo slug={slug} />
       </FeedWrapper>
     </div>
   );
