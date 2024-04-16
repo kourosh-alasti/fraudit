e4245e6c2cf7eec39c8a70931032205a8a426910
Kourosh Alasti <coding@kouroshalasti.com>
2024-04-16 16:04:37 -0700

    Update create-fraudit.ts
    remove imports, and fix error handling

--- src/actions/fraudit/create-fraudit.ts
+++ src/actions/fraudit/create-fraudit.ts
@@ -1,9 +1,10 @@
 "use server";
 
 import db from "@/db/drizzle";
-import { fraudits, userToFraudits } from "@/db/schema";
+import { fraudits } from "@/db/schema";
 import { currentUser } from "@clerk/nextjs";
 import { addUserMembership } from "../user/add-user-membership";
+import { NeonDbError } from "@neondatabase/serverless";
 
 interface Props {
   title: string;
@@ -32,7 +33,18 @@ export const createFraudit = async ({ title, description, slug }: Props) => {
       userId: user.id,
     });
   } catch (err) {
+    if (err instanceof NeonDbError) {
+      console.error(err.message);
+
+      if (err.message.includes("fraudits_slug_unique")) {
+        throw new Error("Fraudit with that slug exists");
+      }
+
+      if (err.message.includes("fraudits_title_unique")) {
+        throw new Error("Frauit with that title exists");
+      }
+    }
     console.error(err);
-    throw new Error("Error Creating a New Fraudit");
+    throw new Error("Something went wrong, please try again later");
   }
 };
