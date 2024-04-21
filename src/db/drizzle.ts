9f784ed236adef5f222f0c17bb59de7f8486f5da
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-21 14:27:01 -0700

    Update drizzle.ts

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,9 +1,15 @@
+import * as dotenv from "dotenv";
+
+dotenv.config({
+  path: [".env.local", ".env"],
+});
+
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-const sql = neon(process.env.NEON_DATABASE_URL!);
+const sql = neon(process.env.NEXT_PUBLIC_NEON_URL!);
 const db = drizzle(sql, { schema });
 
 export default db;
