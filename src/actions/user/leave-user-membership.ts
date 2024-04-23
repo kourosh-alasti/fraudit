5fb6f3029892451ca92b244b06226cfa18d34322
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-23 04:49:37 -0700

    Create leave-user-membership.ts
    leave fraudit action

---
+++ src/actions/user/leave-user-membership.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import db from "@/db/drizzle";
+import { fraudits, userToFraudits } from "@/db/schema";
+import { currentUser } from "@clerk/nextjs";
+import { and, eq } from "drizzle-orm";
+
+interface Props {
+  slug: string;
+}
+
+export const leaveUserMembership = async ({ slug }: Props) => {
+  const user = await currentUser();
+
+  try {
+    if (!user) {
+      throw new Error("Unauthorized Access");
+    }
+
+    const fraudit = await db.query.fraudits.findFirst({
+      where: eq(fraudits.slug, slug),
+    });
+
+    if (!fraudit) {
+      throw new Error("Fraudit does not exist");
+    }
+
+    await db
+      .delete(userToFraudits)
+      .where(
+        and(
+          eq(userToFraudits.userId, user.id),
+          eq(userToFraudits.frauditId, fraudit.id),
+        ),
+      );
+  } catch (err) {
+    console.error(err);
+    throw new Error("Something went wrong. Please try again later!");
+  }
+};
