5a5c7d160d8da303e627fb4e09ddcab90b529c36
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-23 04:50:19 -0700

    Update create-thread-card.tsx
    create thread card dynamic page

--- src/components/fraudit/create-thread-card.tsx
+++ src/components/fraudit/create-thread-card.tsx
@@ -8,19 +8,35 @@ import { addUserMembership } from "@/actions/user/add-user-membership";
 import { Router } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { auth, currentUser } from "@clerk/nextjs";
+import { isMemberOfFraudit } from "@/actions/user/is-member-of-fraudit";
+import { leaveUserMembership } from "@/actions/user/leave-user-membership";
 
 interface Props {
   slug: string;
 }
 
 export const CreateThreadCard = ({ slug }: Props) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [isMember, setIsMember] = useState(false);
   const router = useRouter();
 
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [id, setId] = useState<string | null>(null);
+  useEffect(() => {
+    const getData = () => {
+      setIsLoading(true);
+      isMemberOfFraudit(slug)
+        .then((res) => {
+          if (res) {
+            setIsMember(true);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
 
-  useEffect(() => {}, []);
+    getData();
+    setIsLoading(false);
+  }, []);
 
   const join = () => {
     addUserMembership({ slug: slug })
@@ -31,31 +47,46 @@ export const CreateThreadCard = ({ slug }: Props) => {
       .catch((err) => console.error(err || "An Error Occured"));
   };
 
+  const leave = () => {
+    leaveUserMembership({ slug })
+      .then(() => setIsMember(false))
+      .catch((err) => console.error(err || "An Error Occured"));
+  };
+
   return (
     <>
-      {/* {isLoading && <CreateThreadCardSkeleton />} */}
-      {/* {!isLoading && ( */}
-      <div className="flex h-20 w-full items-center justify-center gap-4 rounded-md border shadow-xl ">
-        {isMember && (
-          <>
-            <div className="h-8 w-auto rounded-md border bg-white px-2 py-1 hover:cursor-pointer hover:bg-gray-100">
-              <Link href={`/app/create/thread?slug=${slug}`}>Create Post</Link>
+      {isLoading && <CreateThreadCardSkeleton />}
+      {!isLoading && isMember && (
+        <div className="flex h-20 w-full items-center justify-center gap-4 rounded-md border shadow-xl ">
+          {isMember && (
+            <>
+              <div className="h-8 w-auto rounded-md border bg-white px-2 py-1 hover:cursor-pointer hover:bg-gray-100">
+                <Link href={`/app/create/thread?slug=${slug}`}>
+                  Create Post
+                </Link>
+              </div>
+              <div
+                className="h-8 w-auto rounded-md bg-rose-500 px-2 py-1 text-white hover:cursor-pointer hover:bg-rose-700"
+                onClick={leave}
+              >
+                Leave Fraudit
+              </div>
+            </>
+          )}
+        </div>
+      )}
+      {!isLoading && !isMember && (
+        <div className="flex h-20 w-full items-center justify-center gap-4 rounded-md border shadow-xl ">
+          {!isMember && (
+            <div
+              className="mx-10 h-8 w-full rounded-md bg-green-500 px-2 py-1 text-center text-white hover:cursor-pointer hover:bg-green-700"
+              onClick={join}
+            >
+              Join Fraudit
             </div>
-            <div className="h-8 w-auto rounded-md bg-rose-500 px-2 py-1 text-white hover:cursor-pointer hover:bg-rose-700">
-              Leave Fraudit
-            </div>
-          </>
-        )}
-        {!isMember && (
-          <div
-            className="mx-10 h-8 w-full rounded-md bg-green-500 px-2 py-1 text-center text-white hover:cursor-pointer hover:bg-green-700"
-            onClick={join}
-          >
-            Join Fraudit
-          </div>
-        )}
-      </div>
-      {/* )} */}
+          )}
+        </div>
+      )}
     </>
   );
 };
