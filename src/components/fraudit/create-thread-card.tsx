ab5877273aaf3ca7e1c9497e251f47a3bee9fa3a
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-23 22:38:32 -0700

    Update create-thread-card.tsx
    dynamic card for leaving or deleting fraudits

--- src/components/fraudit/create-thread-card.tsx
+++ src/components/fraudit/create-thread-card.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { getFrauditBySlug } from "@/db/queries/fraudit";
 import Link from "next/link";
+import { deleteFrauditBySlug } from "@/db/queries/fraudit";
 import { useEffect, useState } from "react";
 import { CreateThreadCardSkeleton } from "./skeletons/create-thread-card-skeleton";
 import { addUserMembership } from "@/actions/user/add-user-membership";
-import { Router } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { auth, currentUser } from "@clerk/nextjs";
 import { isMemberOfFraudit } from "@/actions/user/is-member-of-fraudit";
 import { leaveUserMembership } from "@/actions/user/leave-user-membership";
+import { isOwnerOfFraudit } from "@/actions/user/is-owner-of-fraudit";
+import { Button } from "../ui/button";
+import { useToast } from "../ui/use-toast";
 
 interface Props {
   slug: string;
@@ -18,7 +19,10 @@ interface Props {
 export const CreateThreadCard = ({ slug }: Props) => {
   const [isLoading, setIsLoading] = useState(true);
   const [isMember, setIsMember] = useState(false);
+  const [isOwner, setIsOwner] = useState(false);
+
   const router = useRouter();
+  const { toast } = useToast();
 
   useEffect(() => {
     const getData = () => {
@@ -26,7 +30,17 @@ export const CreateThreadCard = ({ slug }: Props) => {
       isMemberOfFraudit(slug)
         .then((res) => {
           if (res) {
-            setIsMember(true);
+            setIsMember(res.isMember);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      isOwnerOfFraudit(slug)
+        .then((res) => {
+          if (res) {
+            setIsOwner(res.isOwner);
           }
         })
         .catch((err) => {
@@ -47,10 +61,49 @@ export const CreateThreadCard = ({ slug }: Props) => {
       .catch((err) => console.error(err || "An Error Occured"));
   };
 
-  const leave = () => {
+  const leaveFraudit = () => {
     leaveUserMembership({ slug })
-      .then(() => setIsMember(false))
-      .catch((err) => console.error(err || "An Error Occured"));
+      .then(() => {
+        toast({
+          title: "Success",
+          description: "Successfully left the fraudit",
+          variant: "success",
+        });
+
+        setIsMember(false);
+      })
+      .catch((err) => {
+        toast({
+          variant: "destructive",
+          title: "An Error Occured",
+          description: err.message || "Failed to leave the fraudit",
+        });
+
+        console.error(err || "An Error Occured");
+      });
+  };
+
+  const deleteFraudit = () => {
+    deleteFrauditBySlug(slug)
+      .then((res) => {
+        toast({
+          variant: "success",
+          title: "Fraudit Deleted",
+          description: "Fraudit has been deleted",
+        });
+
+        router.push("/app");
+      })
+      .catch((err) => {
+        toast({
+          variant: "destructive",
+          title: "An Error Occured",
+          description:
+            err?.message || "Failed to delete Fraudit, please try again later",
+        });
+
+        console.error(err || "An Error Occured");
+      });
   };
 
   return (
@@ -60,17 +113,28 @@ export const CreateThreadCard = ({ slug }: Props) => {
         <div className="flex h-20 w-full items-center justify-center gap-4 rounded-md border shadow-xl ">
           {isMember && (
             <>
-              <div className="h-8 w-auto rounded-md border bg-white px-2 py-1 hover:cursor-pointer hover:bg-gray-100">
+              <Button className="h-8 w-auto rounded-md border bg-white px-2 py-1 hover:cursor-pointer hover:bg-gray-100">
                 <Link href={`/app/create/thread?slug=${slug}`}>
                   Create Post
                 </Link>
-              </div>
-              <div
-                className="h-8 w-auto rounded-md bg-rose-500 px-2 py-1 text-white hover:cursor-pointer hover:bg-rose-700"
-                onClick={leave}
-              >
-                Leave Fraudit
-              </div>
+              </Button>
+              {isMember && !isOwner && (
+                <Button
+                  className="h-8 w-auto rounded-md bg-rose-500 px-2 py-1 text-white hover:cursor-pointer hover:bg-rose-700"
+                  onClick={leaveFraudit}
+                >
+                  Leave Fraudit
+                </Button>
+              )}
+
+              {isMember && isOwner && (
+                <Button
+                  className="h-8 w-auto rounded-md bg-rose-500 px-2 py-1 text-white hover:cursor-pointer hover:bg-rose-700"
+                  onClick={deleteFraudit}
+                >
+                  Delete
+                </Button>
+              )}
             </>
           )}
         </div>
@@ -78,12 +142,12 @@ export const CreateThreadCard = ({ slug }: Props) => {
       {!isLoading && !isMember && (
         <div className="flex h-20 w-full items-center justify-center gap-4 rounded-md border shadow-xl ">
           {!isMember && (
-            <div
+            <Button
               className="mx-10 h-8 w-full rounded-md bg-green-500 px-2 py-1 text-center text-white hover:cursor-pointer hover:bg-green-700"
               onClick={join}
             >
               Join Fraudit
-            </div>
+            </Button>
           )}
         </div>
       )}
