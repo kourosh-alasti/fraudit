e5544abcd183990beef5aa7aec589930b8992867
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-26 05:21:57 -0700

    Update page.tsx
    redirect if logged in user visits their own page

--- src/app/(main)/app/u/[username]/page.tsx
+++ src/app/(main)/app/u/[username]/page.tsx
@@ -1,23 +1,20 @@
 "use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-
-import { ArrowDown, ArrowUp, Loader } from "lucide-react";
+import { ArrowDown, ArrowUp, Loader2 } from "lucide-react";
 import { useEffect, useState } from "react";
-import { User } from "@clerk/nextjs/server";
-import { getUserByUsername } from "@/db/queries/user";
-import { getThreadById } from "@/db/queries/thread";
-import { getUserThreads } from "@/actions/user";
-import { threads, users } from "@/db/schema";
+import { threads } from "@/db/schema";
 import { UserThreadList } from "@/components/user-thread-list";
-import { getUserInfo } from "@/actions/user/get-user-info";
-import { getUserThreadsById } from "@/actions/user/get-user-threads-by-id";
+import { getUserInfo, getUserThreadsById } from "@/actions/user";
+import { useRouter } from "next/navigation";
+import { useClerk } from "@clerk/nextjs";
 
 const ProfilePage = ({ params }: { params: { username: string } }) => {
   const username = params.username;
+  const router = useRouter();
+  const clerk = useClerk();
 
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState<any>();
@@ -26,7 +23,16 @@ const ProfilePage = ({ params }: { params: { username: string } }) => {
   >([]);
 
   useEffect(() => {
-    const getData = () => {
+    if (!clerk.user) {
+    } else {
+      if (clerk?.user.username === username) {
+        router.push("/app/u");
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    const getData = async () => {
       setLoading(true);
 
       getUserInfo(username)
@@ -51,8 +57,11 @@ const ProfilePage = ({ params }: { params: { username: string } }) => {
 
   return (
     <>
-      {/* TODO: SKELETON */}
-      {loading && <Loader className="h-12 w-12 animate-spin" />}
+      {loading && (
+        <div className="flex flex-col items-center justify-center">
+          <Loader2 className="h-12 w-12 animate-spin" />
+        </div>
+      )}
       {!loading && user && (
         <div className="mx-auto flex h-full w-full flex-col rounded-md bg-white  p-4 sm:min-h-[65vh] sm:w-[70vw]">
           <div className="flex items-center justify-between px-6 py-6 sm:px-12 md:items-start">
