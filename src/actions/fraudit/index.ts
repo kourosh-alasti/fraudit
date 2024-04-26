8253302a368bc388e97bab78fec98a46513ade6a
Kourosh Alasti <coding@kouroshalasti.com>
2024-04-25 22:16:33 -0700

    Update index.ts
    export for fraudit actions

--- src/actions/fraudit/index.ts
+++ src/actions/fraudit/index.ts
@@ -2,3 +2,4 @@ export * from "./create-fraudit";
 export * from "./create-thread";
 export * from "./get-fraudit-info-by-slug";
 export * from "./get-user-fraudits";
+export * from "./delete-fraudit-by-slug";
