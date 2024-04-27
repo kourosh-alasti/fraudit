d434ad3c8e924dfa94c17e418b8404f7cc1bc57b
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-26 19:49:09 -0700

    Update reset.ts
    update reset script

--- src/db/reset.ts
+++ src/db/reset.ts
@@ -9,14 +9,28 @@ const db = drizzle(sql, { schema });
 
 const main = async () => {
   try {
-    console.log("Resetting the database");
+    console.log("--------------Resetting the database--------------");
 
+    await db.delete(schema.universities);
+    console.log("---- DELETED UNIVERSITIES ----");
     await db.delete(schema.courses);
+    console.log("---- DELETED COURSES ----");
+    await db.delete(schema.professors);
+    console.log("---- DELETED PROFESSORS ----");
+    await db.delete(schema.professorsToCourses);
+    console.log("---- DELETED PROFESSORS_TO_COURSES ----");
     await db.delete(schema.userToFraudits);
+    console.log("---- DELETED USER_TO_FRAUDITS ----");
     await db.delete(schema.users);
+    console.log("---- DELETED USERS ----");
     await db.delete(schema.fraudits);
+    console.log("---- DELETED FRAUDITS ----");
+    await db.delete(schema.threads);
+    console.log("---- DELETED THREADS ----");
+    await db.delete(schema.comments);
+    console.log("---- DELETED COMMENTS ----");
 
-    console.log("Database Reset Finished");
+    console.log("--------------Database Reset Finished--------------");
   } catch (err) {
     console.error(err);
 
