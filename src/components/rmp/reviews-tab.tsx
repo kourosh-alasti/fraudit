dc4ba8093e3a2022255f894184093069b4941e70
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-29 05:28:55 -0700

    Update reviews-tab.tsx
    fix reviews tab to display reviews

--- src/components/rmp/reviews-tab.tsx
+++ src/components/rmp/reviews-tab.tsx
@@ -2,6 +2,7 @@
 
 import { reviews } from "@/db/schema";
 import { Card } from "../ui/card";
+import { ReviewTabItem } from "./review-tab-item";
 
 interface Props {
   reviews: (typeof reviews.$inferSelect)[];
@@ -15,6 +16,13 @@ export const ReviewsTab = ({ reviews }: Props) => {
           <h1 className="text-lg font-semibold tracking-tight md:text-xl lg:text-2xl">
             No Reviews Found.
           </h1>
+        </div>
+      )}
+      {reviews.length > 0 && (
+        <div className="mt-4 flex w-full flex-col items-center justify-center gap-2">
+          {reviews.map((review) => (
+            <ReviewTabItem key={review.id} review={review} />
+          ))}
         </div>
       )}
     </>
