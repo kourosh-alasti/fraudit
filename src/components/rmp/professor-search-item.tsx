10404ce472c581014595439215a9df35ed2ac56d
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-29 05:28:13 -0700

    Update professor-search-item.tsx
    fix search item

--- src/components/rmp/professor-search-item.tsx
+++ src/components/rmp/professor-search-item.tsx
@@ -2,6 +2,7 @@
 
 import { getUniversityById } from "@/db/queries/university/get-university-by-id";
 import { professors, universities } from "@/db/schema";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 type TESTPROFS = {
@@ -19,39 +20,41 @@ interface Props {
 
 export const ProfessorSearchItem = ({ professor }: Props) => {
   const [loading, setLoading] = useState(true);
-  //   const [uni, setUni] = useState<typeof universities.$inferSelect>();
+  const [uni, setUni] = useState<typeof universities.$inferSelect>();
 
-  //   useEffect(() => {
-  //     const getData = () => {
-  //       setLoading(true);
-  //       getUniversityById(professor.universityId!)
-  //         .then((res) => {
-  //           setUni(res);
-  //           setLoading(false);
-  //         })
-  //         .catch((err) => console.error(err));
-  //     };
+  useEffect(() => {
+    const getData = () => {
+      setLoading(true);
+      getUniversityById(professor.universityId!)
+        .then((res) => {
+          setUni(res);
+          setLoading(false);
+        })
+        .catch((err) => console.error(err));
+    };
 
-  //     getData();
-  //   }, []);
+    getData();
+  }, []);
 
   return (
     <>
       {loading && <div>Loading...</div>}
       {!loading && (
-        <div
+        <Link
           key={professor.id}
-          className="flex w-full justify-between rounded border border-black bg-zinc-50 px-2 py-1 shadow-md hover:shadow-xl"
+          href={`/rmp/university/professor/${professor.id}`}
         >
-          <div className="flex flex-col gap-1">
-            <p className="text-base md:text-lg">{`${professor.firstName} ${professor.lastName} - CSUF`}</p>
-            <p className="text-sm md:text-base">{professor.email}</p>
+          <div className="flex w-full justify-between rounded border border-black bg-zinc-50 px-2 py-1 shadow-md hover:shadow-xl">
+            <div className="flex flex-col gap-1">
+              <p className="text-base md:text-lg">{`${professor.firstName} ${professor.lastName} - CSUF`}</p>
+              <p className="text-sm md:text-base">{professor.email}</p>
+            </div>
+            <div className="flex flex-col gap-1">
+              <p className="text-xs md:text-sm">Rating</p>
+              <p>{`${professor.rating?.toFixed(1)} / 5.0`}</p>
+            </div>
           </div>
-          <div className="flex flex-col gap-1">
-            <p className="text-xs md:text-sm">Rating</p>
-            <p>{`${professor.rating} / 5.0`}</p>
-          </div>
-        </div>
+        </Link>
       )}
     </>
   );
