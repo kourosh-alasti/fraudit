65f4824458e09ed3295ceda6ceaca956b6211eb1
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-29 01:33:17 -0700

    Create reviews-tab.tsx
    Review tab need too add review info

---
+++ src/components/rmp/reviews-tab.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { reviews } from "@/db/schema";
+import { Card } from "../ui/card";
+
+interface Props {
+  reviews: (typeof reviews.$inferSelect)[];
+}
+
+export const ReviewsTab = ({ reviews }: Props) => {
+  return (
+    <>
+      {reviews.length === 0 && (
+        <div className="mt-4 flex w-full flex-col items-center justify-center">
+          <h1 className="text-lg font-semibold tracking-tight md:text-xl lg:text-2xl">
+            No Reviews Found.
+          </h1>
+        </div>
+      )}
+    </>
+  );
+};
