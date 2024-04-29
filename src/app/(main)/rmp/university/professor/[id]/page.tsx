3d0e1f7c0dd88dc0eef855883e06c5a0ce7f9f66
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-29 05:27:46 -0700

    Update page.tsx
    make some changes to professor home page

--- src/app/(main)/rmp/university/professor/[id]/page.tsx
+++ src/app/(main)/rmp/university/professor/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ProfessorTabs } from "@/components/rmp/professor-tabs";
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { getProfessorById } from "@/db/queries/professors/get-professor-by-id";
 import {
@@ -10,6 +11,7 @@ import {
   universities,
 } from "@/db/schema";
 import { Loader2 } from "lucide-react";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import StarRatings from "react-star-ratings";
 
@@ -51,13 +53,20 @@ const ProfessorPage = ({ params: { id } }: { params: { id: string } }) => {
 
       {!loading && prof && (
         <div className="flex flex-col gap-4">
+          <div className="flex justify-between">
+            <div className="flex flex-col">
+              <h1 className="text-lg font-semibold md:text-xl lg:text-2xl">
+                {`${prof.firstName} ${prof.lastName}`}
+              </h1>
+              <h3 className="text-base font-bold tracking-wide text-slate-700 md:text-lg lg:text-xl">
+                {prof.email}
+              </h3>
+            </div>
+            <Link href={`/rmp/university/professor/review?id=${professorId}`}>
+              <Button variant={"secondary"}>Review Me</Button>
+            </Link>
+          </div>
           <div className="flex flex-col">
-            <h1 className="text-lg font-semibold md:text-xl lg:text-2xl">
-              {`${prof.firstName} ${prof.lastName}`}
-            </h1>
-            <h3 className="text-base font-bold tracking-wide text-slate-700 md:text-lg lg:text-xl">
-              {prof.email}
-            </h3>
             <div className="flex flex-col md:max-w-[988px] md:flex-row md:items-center md:justify-between">
               <p className=" mb-2 mt-6 text-xs text-muted-foreground md:text-sm lg:text-base">
                 Professor Rating
