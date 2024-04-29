2d45c310862ff601e56fbe8ad9e5b85c3d78087d
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-29 01:32:50 -0700

    Create search-bar.tsx
    Search Bar

---
+++ src/components/rmp/search-bar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Input } from "../ui/input";
+import Link from "next/link";
+import { useState } from "react";
+import { Button } from "../ui/button";
+
+interface Props {
+  className?: string;
+  value?: string;
+}
+
+export const SearchBar = ({ className, value }: Props) => {
+  const [searchValue, setSearchValue] = useState("");
+
+  return (
+    <div className={cn(className)}>
+      <Input
+        type="text"
+        placeholder="Find a Professor"
+        value={value || searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        className="w-64 rounded-l-md rounded-r-none border-slate-500 bg-transparent px-3 py-2 text-gray-500 focus:outline-none active:outline-none"
+      />
+      <Link href={`/rmp/search?q=${searchValue}`}>
+        <Button className="rounded-l-none hover:cursor-pointer hover:bg-gray-900 active:bg-gray-800">
+          Search
+        </Button>
+      </Link>
+    </div>
+  );
+};
