3a8ff844ab7095894691426beaed02792608db52
Kourosh Alasti <coding@kouroshalasti.com>
2024-12-19 22:07:01 -0800

    lint: fix lint

--- src/app/(main)/app/f/[slug]/[threadId]/page.tsx
+++ src/app/(main)/app/f/[slug]/[threadId]/page.tsx
@@ -6,10 +6,8 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { getThreadById } from "@/db/queries/thread";
-import { getUser } from "@/db/queries/user";
-import { threads, users, comments } from "@/db/schema";
+import { threads, comments } from "@/db/schema";
 import { ArrowDownIcon, ArrowUpIcon, Loader2 } from "lucide-react";
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { CommentModal } from "@/components/comment-modal";
 import { getUserInfoById } from "@/actions/user";
@@ -21,7 +19,6 @@ const FrauditThreadPage = ({
 }: {
   params: { threadId: string };
 }) => {
-  const router = useRouter();
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState<any>();
   const [thread, setThread] = useState<typeof threads.$inferSelect>();
@@ -67,7 +64,7 @@ const FrauditThreadPage = ({
         <div className="flex items-center justify-center">Thread not found</div>
       )}
       {!isLoading && thread && user && (
-        <div className="flex min-h-[40%] w-full flex-col items-center justify-center gap-12  px-4 py-2 text-black">
+        <div className="flex min-h-[40%] w-full flex-col items-center justify-center gap-12 px-4 py-2 text-black">
           <div className="mx-auto flex w-full flex-col rounded-md border bg-gray-100 px-4 py-2 shadow-md lg:max-w-[988px]">
             <div className="mb-3 flex w-full items-center justify-between">
               <h1 className="text-lg md:text-xl lg:text-3xl">{thread.title}</h1>
@@ -77,18 +74,18 @@ const FrauditThreadPage = ({
             </div>
             <Separator className="hidden h-[1px] bg-slate-800 text-slate-800 md:block" />
 
-            <div className=" mt-2 flex w-full items-center md:mt-4">
+            <div className="mt-2 flex w-full items-center md:mt-4">
               <div className="markdown-body">
                 <Markdown>{thread.content}</Markdown>
               </div>
             </div>
           </div>
           <div className="flex w-full flex-col gap-2 px-2 md:flex-row-reverse md:gap-[48px]">
-            <FeedWrapper className=" md:max-w-[250px]">
+            <FeedWrapper className="md:max-w-[250px]">
               <CommentModal threadId={thread.id} frauditId={thread.frauditId!}>
                 <Button>Comment</Button>
               </CommentModal>
-              <div className="flex w-full gap-4 ">
+              <div className="flex w-full gap-4">
                 <Button
                   aria-disabled="true"
                   disabled={true}
@@ -108,7 +105,7 @@ const FrauditThreadPage = ({
 
             <div className="flex-1">
               <FeedWrapper>
-                <div className="mx-auto flex w-full flex-col rounded-md border bg-gray-100 px-4 py-2 shadow-md lg:max-w-[988px] ">
+                <div className="mx-auto flex w-full flex-col rounded-md border bg-gray-100 px-4 py-2 shadow-md lg:max-w-[988px]">
                   <h1 className="mb-1 text-lg font-semibold md:text-xl lg:text-2xl xl:text-4xl">
                     Comments
                   </h1>
